f2685cbbe0b5e8095dd965fce3f3306a151c6211
Aleks Grig <ullitap@gmail.com>
2019-08-15 12:31:07 +0300

    Initial commit

---
+++ Thrillio/src/com/aurelius/thrillio/entities/User.java
@@ -0,0 +1,12 @@
+package com.aurelius.thrillio.entities;
+
+public class User {
+
+	private long id;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private int gender;
+	private String userType;
+}
